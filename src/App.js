c8e12256d8202770fc87d499ea1ea7c24fa895c9
Vicente Borjas <vrborjasm@gmail.com>
2020-08-18 10:36:06 -0700

    Use googlemap component

--- src/App.js
+++ src/App.js
@@ -7,9 +7,34 @@ import {
   InfoWindow
 } from "@react-google-maps/api";
 
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: "100vw",
+  height: "100vh"
+}
+const center = {
+  lat: -33.4372,
+  lng: -70.6506
+}
+
 function App() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+
+  if (loadError) return "Error loading maps";
+  if (!isLoaded) return "Loading Maps";
+
   return (
-    <div>map</div>
+    <div>
+      <GoogleMap 
+        mapContainerStyle={mapContainerStyle} 
+        zoom={8}
+        center={center}
+        >          
+        </GoogleMap>
+    </div>
   );
 }
 
