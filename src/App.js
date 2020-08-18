f7878ff0868b0c49d967fe3310b3674755a719ec
Vicente Borjas <vrborjasm@gmail.com>
2020-08-18 12:19:52 -0700

    Create function addMarkers

--- src/App.js
+++ src/App.js
@@ -1,39 +1,59 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import {
   GoogleMap,
   useLoadScript,
   Marker,
-  InfoWindow
+  InfoWindow,
 } from "@react-google-maps/api";
 
 const libraries = ["places"];
 const mapContainerStyle = {
   width: "100vw",
-  height: "100vh"
-}
+  height: "100vh",
+};
 const center = {
   lat: -33.4372,
-  lng: -70.6506
-}
+  lng: -70.6506,
+};
 
 function App() {
   const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    googleMapsApiKey: "AIzaSyAeuN7UAZwpM7WLI8G217kTZ5dx3vE66oY",
     libraries,
   });
+
+  const [markers, setMarkers] = useState([]);
 
   if (loadError) return "Error loading maps";
   if (!isLoaded) return "Loading Maps";
 
+  const addMarkers = (e) => {
+    setMarkers((markers) => [
+      ...markers,
+      {
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  };
+
   return (
     <div>
-      <GoogleMap 
-        mapContainerStyle={mapContainerStyle} 
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
         zoom={8}
         center={center}
-        >          
-        </GoogleMap>
+        onClick={addMarkers}
+      >
+        {markers.map((marker) => (
+          <Marker
+            key={marker.time.toISOString()}
+            position={{ lat: marker.lat, lng: marker.lng }}
+          />
+        ))}
+      </GoogleMap>
     </div>
   );
 }
